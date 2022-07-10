3783f8df1ddc1527d2af797fd9d251779da290ed
yaswanth-vakkala <yaswanthv87@gmail.com>
2022-07-10 20:39:30 +0530

    add n*n grid functionallity to the sketching area

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const drawArea = document.querySelector(".draw-area");
 
 function generateArea() {
   for (let i = 0; i < 16; i++) {
-    const division = document.createElement("div");
-    drawArea.appendChild(division);
+    for (let i = 0; i < 16; i++) {
+      const division = document.createElement("div");
+      drawArea.appendChild(division);
+    }
   }
 }
 
