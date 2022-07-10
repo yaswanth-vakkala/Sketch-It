53ab737ade17663291d029f62f3c188ea06fa9e2
yaswanth-vakkala <yaswanthv87@gmail.com>
2022-07-10 20:28:31 +0530

    complete the layout of the website

---
+++ script.js
@@ -0,0 +1,10 @@
+const drawArea = document.querySelector(".draw-area");
+
+function generateArea() {
+  for (let i = 0; i < 16; i++) {
+    const division = document.createElement("div");
+    drawArea.appendChild(division);
+  }
+}
+
+generateArea();
