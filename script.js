ea8ab4c0c7e018662162298eedd65b2e1109681d
yaswanth-vakkala <yaswanthv87@gmail.com>
2022-07-13 14:20:32 +0530

    refactor the code -1

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function generateArea(squares = 16) {
     division.style.width = 512 / squares + "px";
     drawArea.appendChild(division);
   }
-  //   addEL(squares);
 }
 
 function addElBtn() {
@@ -25,23 +24,6 @@ function addElBtn() {
 
 let flag = 0;
 let clicked = 0;
-
-// document.body.ondragover = function (e) {
-//   e.preventDefault();
-// };
-
-// document.body.onmousedown = function () {
-//   ++clicked;
-// };
-// document.body.onmousemove = function () {
-//   if (clicked == 1) {
-//     clicked = 1;
-//   }
-// };
-// document.body.onmouseup = function () {
-//   --clicked;
-// };
-
 let color = "#000000";
 let mflag = 0;
 let eflag = 0;
@@ -57,34 +39,16 @@ window.addEventListener("mouseup", function () {
 });
 
 function addEL(squares = 16) {
-  // addEventListener("onmousedown", mDown(this));
   const drawAreaElements = document.querySelectorAll(".draw-area div");
   for (let i = 0; i < squares * squares; i++) {
-    // drawAreaElements[i].addEventListener("mousedown", function () {
-    //   mflag = 1;
-    // });
-
-    // drawAreaElements[i].addEventListener("mouseup", function () {
-    //   mflag = 0;
-    // });
     drawAreaElements[i].addEventListener("mouseover", function () {
-      // if (clicked == 1) {
-
       if (flag === 0 && mflag === 1) {
         this.style.backgroundColor = color;
       } else if (flag === 1 && eflag === 1 && mflag == 0) {
         this.style.backgroundColor = "#ffffff";
       }
-      // }
     });
-    // drawAreaElements[i].addEventListener("mousedown", function () {
-    //   clicked = 1;
-    // });
-    // drawAreaElements[i].addEventListener("mouseup", function () {
-    //   clicked = 0;
-    // });
   }
-  //   console.log(flag);
 }
 
 generateArea(16);
@@ -93,18 +57,10 @@ addElBtn();
 
 let input;
 
-// function handleInput() {
-//   input = prompt("Enter no of squares for the grid: ");
-//   drawArea.replaceChildren();
-//   generateArea(input);
-//   addEL(input);
-// }
-
 function handleClear() {
   drawArea.replaceChildren();
   generateArea(input);
   addEL(input);
-  // addElBtn();
 }
 
 function handleEraser() {
@@ -118,30 +74,10 @@ function handlePen() {
   eflag = 0;
 }
 
-// function addEL2() {
-//     // addEventListener("onmousedown", mDown(this));
-//     const drawAreaElements = document.querySelectorAll(".draw-area div");
-//     for (let i = 0; i < 255; i++) {
-//       drawAreaElements[i].addEventListener("mouseup", function () {
-//       //   this.style.backgroundColor = '#000000';
-//       flat = 0;
-//       });
-//     }
-//     console.log(flag)
-//   }
-// addEL2();
-
-// function mDown(obj) {
-//   // obj.style.backgroundColor("#000000");
-//   // obj.classList.add("active-state");
-//   console.log(obj);
-// }
-
 let slider = document.getElementById("myRange");
 let output = document.getElementById("slider-output");
 output.innerHTML = slider.value + " x " + slider.value;
 
-// Update the current slider value (each time you drag the slider handle)
 slider.oninput = function () {
   output.innerHTML = this.value + " x " + this.value;
   drawArea.replaceChildren();
